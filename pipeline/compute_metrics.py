a697388c4394a115c6f8be791c85c82ebdfdb4fd
Skillshetra <skillshetra@gmail.com>
2026-02-02 19:54:04 +0530

    Compute Metrics

--- pipeline/compute_metrics.py
+++ pipeline/compute_metrics.py
@@ -8,17 +8,17 @@ This module contains the compute_all_metrics class which computes
 various metrics from the provided dataframe.
 """
 
+
 # Import necessary packages #
-from stats.confidence_intervals import *
-from stats.hypothesis import *
-from stats.multiple_testing import *
-from stats.power import *
-from stats.sequential import *
-from metrics.aggregations import *
-from metrics.base import *
-from metrics.cuped import *
-from causal.regression import *
-from causal.diff_in_diff import *
+from stats.confidence_intervals import confidence_intervals
+from stats.hypothesis import hypothesis_tests
+from stats.multiple_testing import multiple_testing_correction
+from stats.power import power_analysis
+from stats.sequential import sequential_analysis
+from metrics.aggregations import aggregations
+from metrics.cuped import cuped_regression_analysis
+from causal.regression import regression_causal_effects
+from causal.diff_in_diff import diff_in_diff_causal_effects
 
 
 class compute_all_metrics:
@@ -29,26 +29,46 @@ class compute_all_metrics:
     """
     def __init__(self, dataframe):
         self.dataframe = dataframe
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n") # Remove all this
+        self.results = {}
     
     # Function to get all Metric values from dataset #
     def get_metrics(self):
-        metrics = 0
-        return metrics
 
-# Creating Dataframe #
-# dataframe = get_products()
-# print(dataframe.info())
-# print(dataframe.describe())
-# print(dataframe.isnull().sum())
+        # Confidence Intervals #
+        print("\033[34m[INFO] Computing Confidence Intervals.\033[0m")
+        self.results['confidence_intervals'] = confidence_intervals(self.dataframe)
 
-# print(dataframe['product_id'])
-# print(dataframe['category'])
-# print(dataframe['discounted_price'])
-# print(dataframe['actual_price'])
-# print(dataframe['discount_percentage'])
-# print(dataframe['rating'])
-# print(dataframe['rating_count'])
-# print(dataframe['user_id'])
-# print(dataframe['user_name'])
-# print(dataframe['review_id'])
+        # Hypothesis Testing #
+        print("\033[34m[INFO] Computing Hypothesis Tests.\033[0m")
+        self.results['hypothesis_tests'] = hypothesis_tests(self.dataframe)
+
+        # Multiple Testing Correction #
+        print("\033[34m[INFO] Applying Multiple Testing Correction.\033[0m")
+        self.results['corrected_p_values'] = multiple_testing_correction(self.results['hypothesis_tests'])
+
+        # Power Analysis #
+        print("\033[34m[INFO] Computing Power Analysis.\033[0m")
+        self.results['power_analysis'] = power_analysis(self.dataframe)
+
+        # Sequential Analysis #
+        print("\033[34m[INFO] Performing Sequential Analysis.\033[0m")
+        self.results['sequential_results'] = sequential_analysis(self.dataframe)
+
+        # Aggregations #
+        print("\033[34m[INFO] Computing Aggregations.\033[0m")
+        self.results['aggregations'] = aggregations(self.dataframe)
+
+        # Cuped Regression #
+        print("\033[34m[INFO] Performing Cuped Regression Analysis.\033[0m")
+        self.results['cuped_results'] = cuped_regression_analysis(self.dataframe)
+
+        # Causal Inference - Regression #
+        print("\033[34m[INFO] Computing Causal Effects using Regression.\033[0m")
+        self.results['regression_causal_effects'] = regression_causal_effects(self.dataframe)
+
+        # Causal Inference - Difference in Differences #
+        print("\033[34m[INFO] Computing Causal Effects using Difference in Differences.\033[0m")
+        self.results['diff_in_diff_causal_effects'] = diff_in_diff_causal_effects(self.dataframe)
+
+        # Returning Results #
+        return self.results
